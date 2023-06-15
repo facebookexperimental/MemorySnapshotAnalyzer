c08b333b4a673f0ba71e1b50c627f4069d64bc1d
Ellie Kornstaedt <ellieko@meta.com>
2023-06-15 08:36:20 -0700

    Added ability to list object instances in heapdomstats, to support finding where more owning references need to be identified

--- Commands/HeapDomStatsCommand.cs
+++ Commands/HeapDomStatsCommand.cs
@@ -1,9 +1,11 @@
 ﻿// Copyright(c) Meta Platforms, Inc. and affiliates.
 
+using MemorySnapshotAnalyzer.AbstractMemorySnapshot;
 using MemorySnapshotAnalyzer.CommandProcessing;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace MemorySnapshotAnalyzer.Commands
 {
@@ -11,9 +13,31 @@ namespace MemorySnapshotAnalyzer.Commands
     {
         public HeapDomStatsCommand(Repl repl) : base(repl) {}
 
+#pragma warning disable CS0649 // Field '...' is never assigned to, and will always have its default value
+        [FlagArgument("list")]
+        public bool List;
+
+        [NamedArgument("type")]
+        public int TypeIndex = -1;
+
+        [FlagArgument("memory")]
+        public bool Memory;
+#pragma warning restore CS0649 // Field '...' is never assigned to, and will always have its default value
+
         public override void Run()
         {
-            int rootNodeIndex = CurrentHeapDom.RootNodeIndex;
+            if (List)
+            {
+                ListToplevelObjects();
+            }
+            else
+            {
+                DumpStats();
+            }
+        }
+
+        void DumpStats()
+        {
             List<int>? children = CurrentHeapDom.GetChildren(CurrentHeapDom.RootNodeIndex);
 
             var stats = new Dictionary<int, (int Count, long Size)>();
@@ -62,6 +86,42 @@ namespace MemorySnapshotAnalyzer.Commands
             }
         }
 
-        public override string HelpText => "heapdomstats";
+        void ListToplevelObjects()
+        {
+            List<int>? children = CurrentHeapDom.GetChildren(CurrentHeapDom.RootNodeIndex);
+            if (children != null)
+            {
+                var sb = new StringBuilder();
+                for (int i = 0; i < children.Count; i++)
+                {
+                    int nodeIndex = children[i];
+                    if (CurrentBacktracer.IsLiveObjectNode(nodeIndex))
+                    {
+                        int postorderIndex = CurrentBacktracer.NodeIndexToPostorderIndex(nodeIndex);
+                        int typeIndex = CurrentTracedHeap.PostorderTypeIndexOrSentinel(postorderIndex);
+
+                        if (TypeIndex != -1 && typeIndex == TypeIndex || TypeIndex == -1 && typeIndex != -1)
+                        {
+                            NativeWord address = CurrentTracedHeap.PostorderAddress(postorderIndex);
+                            DescribeAddress(address, sb);
+                            Output.WriteLine(sb.ToString());
+                            sb.Clear();
+
+                            if (Memory)
+                            {
+                                SegmentedHeap? segmentedHeap = CurrentSegmentedHeapOpt;
+                                if (segmentedHeap != null)
+                                {
+                                    MemoryView objectView = segmentedHeap.GetMemoryViewForAddress(address);
+                                    DumpObjectMemory(address, objectView);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public override string HelpText => "heapdomstats ['list ['type <type index>] ['memory]]";
     }
 }
